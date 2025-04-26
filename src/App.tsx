cce510366a79bf976c2b2bb5204b1b21a4ceb7f1
Shaheed mon <sheheedk07@gmail.com>
2025-04-26 11:16:49 +0530

    ongoing

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,26 @@ function App() {
 
   return (
     <>
-      <div className="card">
-        <input type="color" onChange={(e) => setColor(e.target.value)} />
-        <button onClick={onClickHandler}>
-          click me
-        </button>
+      <div className="container">
+        <h1 className="title">Quick change</h1>
+        <p className="subtitle">Pick a color and apply it to your current tab!</p>
+
+        <div className="preview-box" style={{ backgroundColor: color || '#ffffff' }}>
+          <span>{color || 'No Color Selected'}</span>
+        </div>
+
+        <div className="card">
+          <label htmlFor="colorPicker" className='choose'>Choose a color :</label>
+          <input
+            id="colorPicker"
+            type="color"
+            onChange={(e) => setColor(e.target.value)}
+          />
+
+          <button onClick={onClickHandler}>
+            Apply Background
+          </button>
+        </div>
       </div>
     </>
   );
